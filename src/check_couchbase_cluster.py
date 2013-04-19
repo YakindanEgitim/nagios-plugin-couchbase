15191c1e0cac2f5c8b9102c63eb8cd112c074e0c
Ebru Akagündüz <ebru.akagunduz@gmail.com>
2013-04-19 20:15:06 +0300

    added a feature retrieving a bucket information from cluster

---
+++ src/check_couchbase_cluster.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from optparse import OptionParser
+import requests
+import json
+
+parser = OptionParser()
+#options
+parser.add_option('-I', dest='ip')
+parser.add_option('-u', dest='username')
+parser.add_option('-p', dest='password')
+parser.add_option('-b', dest='bucket')
+options, args = parser.parse_args()
+
+try:
+	url = ''.join(['http://', options.ip, '/pools/', options.bucket])
+	r = requests.get(url, auth=(options.username, options.password))
+	result = r.json()
+	for key, values in result.items():
+		print key,": ", values
+except Exception:
+	print "Invalid option combination"
+	print "Try '--help' for more information "
