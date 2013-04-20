20566049192aa8e251f38a6f968ee37b00a6b4b3
Ebru Akagündüz <ebru.akagunduz@gmail.com>
2013-04-20 23:33:04 +0300

    added basic setup.py related #9

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+ 
+setup(
+	name = "Couchbase Nagios Plugin",
+    version = "1.0",
+	description = "A simple nagios plugin to monitor Couchbase 2.0 servers/cluster.",
+	author = "Ebru Akagunduz",
+	author_email = "ebru.akagunduz@gmail.com",
+	packages = ['src'],
+	scripts = ['src/check_couchbase_cluster.py', 'src/check_couchbase_server.py'],
+)
+
+
