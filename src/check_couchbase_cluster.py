5562bb25487948fe5667714908a6888b26a7542a
Ebru Akagündüz <ebru.akagunduz@gmail.com>
2013-04-20 09:48:14 +0300

    added a little code for operation per second check

--- src/check_couchbase_cluster.py
+++ src/check_couchbase_cluster.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python
 from optparse import OptionParser
 import requests
+import sys
 import json
 
+#option required none
+def option_none(option, opt, value, parser):
+	if  parser.rargs and not parser.rargs[0].startswith('-'):
+		print "Option arg error"
+		print opt, " option should be empty"
+		sys.exit(2)
+
 parser = OptionParser()
+parser.disable_interspersed_args()
 #options
 parser.add_option('-I', dest='ip')
 parser.add_option('-u', dest='username')
 parser.add_option('-p', dest='password')
 parser.add_option('-b', dest='bucket')
+parser.add_option('--OPS', action='callback', callback=option_none)
+
 options, args = parser.parse_args()
 
 try:
@@ -17,7 +28,6 @@ try:
 	result = r.json()
 	result = json.dumps(result)
 	print result
-
 except Exception:
 	print "Invalid option combination"
 	print "Try '--help' for more information "
--- src/check_couchbase_server.py
+++ src/check_couchbase_server.py
@@ -3,13 +3,22 @@ from optparse import OptionParser
 import requests
 import json
 
+#option required none
+def option_none(option, opt, value, parser):
+    if  parser.rargs and not parser.rargs[0].startswith('-'):
+        print "Option arg error"
+        print opt, " option should be empty"
+        sys.exit(2)
+
 parser = OptionParser()
+parser.disable_interspersed_args()
 #options
 parser.add_option('-I', dest='ip')
 parser.add_option('-s', dest='server')
 parser.add_option('-u', dest='username')
 parser.add_option('-p', dest='password')
 parser.add_option('-b', dest='bucket')
+parser.add_option('--OPS', action='callback', callback=option_none)
 options, args = parser.parse_args()
 
 try:
@@ -17,7 +26,10 @@ try:
 	r = requests.get(url, auth=(options.username, options.password))
 	result = r.json()
 	result = json.dumps(result)
+#	print type(result)
+#	print result.split("\"opsPerSec\":")
 	print result
+#	result.index()
 
 except Exception:
     print "Invalid option combination"
