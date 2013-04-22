f7f01b0afe54883f93b3aee6ffddb41f730d89e0
Ebru Akagündüz <ebru.akagunduz@gmail.com>
2013-04-23 02:52:33 +0300

    added disk read per second check related #14

--- src/check_couchbase_cluster.py
+++ src/check_couchbase_cluster.py
@@ -30,6 +30,11 @@ def check_mem_usage(result):
 	interestingStats = nodes['interestingStats']	
 	print "interestingStats MemUsed: ", interestingStats['mem_used']
 
+def check_disk_read(result):
+	result = result[0]
+	basicStats = result['basicStats']
+	print "diskFetches: ", basicStats['diskFetches']
+
 parser = OptionParser()
 parser.disable_interspersed_args()
 #options
@@ -39,6 +44,7 @@ parser.add_option('-p', dest='password')
 parser.add_option('-b', dest='bucket')
 parser.add_option('--OPS', action='callback', callback=option_none, dest='operations_per_second')
 parser.add_option('--mem', action='callback', callback=option_none, dest='memoryUsage')
+parser.add_option('--disk-read', action='callback', callback=option_none, dest='disk_read')
 options, args = parser.parse_args()
 
 try:
@@ -49,6 +55,8 @@ try:
 		check_ops_per_second(result)
 	if options.memoryUsage:
 		check_mem_usage(result)
+	if options.disk_read:
+		check_disk_read(result)
 	else:
 		result = json.dumps(result)
 		print result
