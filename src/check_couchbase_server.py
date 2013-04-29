c230fac682190c22a552ea6ba2b45c00d73d71be
Ebru Akagündüz <ebru.akagunduz@gmail.com>
2013-04-29 10:10:02 +0300

    added warning and critical option

--- src/check_couchbase_server.py
+++ src/check_couchbase_server.py
@@ -4,6 +4,8 @@ import requests
 import json
 import sys
 
+nagios_codes = {'OK': 0, 'WARNING': 1, 'CRITICAL': 2,}
+
 #option required none
 def option_none(option, opt, value, parser):
 	if parser.rargs and not parser.rargs[0].startswith('-'):
@@ -15,7 +17,20 @@ def option_none(option, opt, value, parser):
 def check_ops_per_second(result):
 	# basic bucket stats  from json
 	basicStats = result['basicStats']
-	print "opsPerSec:",  basicStats['opsPerSec']
+#	print "opsPerSecc:: ",  basicStats['opsPerSec']
+	opsPerSec =  basicStats['opsPerSec']
+	options.warning = float(options.warning)
+	options.critical = float(options.critical)
+	if opsPerSec == 0.0 or opsPerSec <= options.warning:
+		print "WARNING"
+		print nagios_codes['WARNING']
+		return sys.exit(nagios_codes['WARNING'])
+	elif opsPerSec <= options.critical:
+		print "CRITICAL"
+		return sys.exit(nagios_codes['CRITICAL'])
+	else:
+		print "OK"
+		return sys.exit(nagios_codes['OK'])
 
 def check_mem_usage(result):
 	basicStats = result['basicStats']
@@ -30,7 +45,8 @@ def check_mem_usage(result):
 
 def check_disk_read(result):
 	basicStats = result['basicStats']
-	print "diskFetches:", basicStats['diskFetches']
+	#print "diskFetches:", basicStats['diskFetches']
+	
 	
 parser = OptionParser()
 parser.disable_interspersed_args()
@@ -42,6 +58,8 @@ parser.add_option('-s', dest='server')
 parser.add_option('-u', dest='username')
 parser.add_option('-p', dest='password')
 parser.add_option('-b', dest='bucket')
+parser.add_option('-W', dest='warning')
+parser.add_option('-C', dest='critical')
 parser.add_option('--OPS', action='callback', callback=option_none, dest='operations_per_second')
 parser.add_option('--mem', action='callback', callback=option_none, dest='memoryUsage')
 parser.add_option('--disk-read', action='callback', callback=option_none, dest='disk_read')
