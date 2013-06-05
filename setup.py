92f4b798abd20ad470dbef8d3d112bd4ce0862fc
Ebru Akagündüz <ebru.akagunduz@gmail.com>
2013-06-05 16:53:16 +0300

    add license info to setup.py

--- setup.py
+++ setup.py
@@ -22,12 +22,13 @@ from distutils.core import setup
 import glob
 
 setup(
-	name = "Couchbase Nagios Plugin",
+	name = "nagios-plugin-couchbase",
 	version = "1.0",
 	description = "A simple nagios plugin to monitor Couchbase 2.0 servers/cluster.",
 	author = "Ebru Akagunduz",
 	author_email = "ebru.akagunduz@gmail.com",
 	packages = ['src'],
-	data_files = [('/usr/lib/nagios/plugins/', glob.glob("src/*py"))]
+	license = "GPLv3"
+	data_files = [('/usr/lib/nagios/plugins/', glob.glob("src/check_couchbase.py"))]
 )
 
